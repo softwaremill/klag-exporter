8ee1a251f2e70836d835cd802c9c898ce79e67af
@x10an14-nav <christian.chavez@nav.no>
2026-06-26 16:21:46 +0200

    devex: add nix tooling for reproducible builds

---
+++ flake.nix
@@ -0,0 +1,79 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ (import inputs.rust-overlay) ];
+        };
+        inherit (pkgs) lib;
+        cargoToml = lib.fromTOML (lib.readFile ./Cargo.toml);
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "clippy"
+            "rust-analyzer"
+            "rust-src"
+            "rustfmt"
+          ];
+        };
+
+        klag-exporter =
+          (pkgs.makeRustPlatform {
+            cargo = rustToolchain;
+            rustc = rustToolchain;
+          }).buildRustPackage
+            {
+              pname = cargoToml.package.name;
+              version = "${cargoToml.package.version}-${
+                if lib.hasAttr "rev" inputs.self then
+                  "${lib.toString inputs.self.revCount}-${inputs.self.shortRev}"
+                else
+                  "gitDirty"
+              }";
+
+              src = lib.cleanSource ./.;
+              cargoLock.lockFile = ./Cargo.lock;
+              LIBCLANG_PATH = "${lib.makeLibraryPath [ pkgs.libclang ]}";
+
+              buildInputs = [
+                pkgs.openssl
+                pkgs.jemalloc
+                pkgs.cyrus_sasl
+                pkgs.curl
+              ];
+              nativeBuildInputs = [
+                pkgs.pkg-config
+                pkgs.cmake
+              ];
+            };
+      in
+      {
+        checks = { inherit klag-exporter; };
+        devShells.default = pkgs.mkShell {
+          hardeningDisable = [ "fortify" ];
+          LIBCLANG_PATH = "${lib.makeLibraryPath [ pkgs.libclang ]}"; # for dev builds
+          inputsFrom = [ klag-exporter ];
+          packages = with pkgs; [
+            cargo-watch
+          ];
+        };
+        packages = {
+          inherit klag-exporter;
+          default = klag-exporter;
+        };
+      }
+    );
+}
